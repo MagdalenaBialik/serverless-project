01932f5f7494000ac95ee734a2a3a82ab16530a2
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-01-04 13:09:58 +0700

    added GHA

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "pets-app-function" {
   function_name = "${var.app_name}-lambda-add-pet"
   role          = var.lambda_role
 
-  s3_bucket = "pets-app-artifact"
+  s3_bucket = "pets-app-artifacts"
   s3_key    = "${var.file_hash}.zip"
 
   handler = "main.handler"
