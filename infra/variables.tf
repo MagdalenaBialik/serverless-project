6a6f05f5d8dc820846da37ec3b0b5de756a5d772
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-01-05 16:57:07 +0700

    added statistics.py with statistic and get_object_from_s3 function

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,14 +6,6 @@ variable "file_hash" {
   type = string
 }
 
-variable "add_pet_function_suffix" {
-  type = string
-}
-
-variable "statistic_function_suffix" {
-  type = string
-}
-
 variable "s3_bucket_artifacts" {
   type = string
 }
