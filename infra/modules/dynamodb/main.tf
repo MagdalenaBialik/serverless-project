80d979ee984be4a12397409e8a1a5404282ec397
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-01-09 20:03:52 +0700

    added aws_lambda_event_source_mapping

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -1,10 +1,11 @@
 resource "aws_dynamodb_table" "dynamodb_table" {
-  name           = "${var.app_name}-statistics-table"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "PK"
-  range_key      = "SK"
-  stream_enabled = true
+  name             = "${var.app_name}-statistics-table"
+  read_capacity    = 5
+  write_capacity   = 5
+  hash_key         = "PK"
+  range_key        = "SK"
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
 
   attribute {
     name = "PK"
