1ce3501e151639393236f325a3e08d25466abe0f
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-01-10 10:42:14 +0700

    test

--- app/dynamodb_stream.py
+++ app/dynamodb_stream.py
@@ -1,13 +1,21 @@
 import json
+from logging import getLogger
 
 from app.statistics import StatisticsSettings, ses_send
 
+logger = getLogger()
 settings = StatisticsSettings()
+#
+# def get_pet_name_from_stream_event(event):
+#     image = event["Records"][0]["dynamodb"]["NewImage"]
+#     name = image["PK"]["S"]
 
 
 def handler(event, context):
     image = event["Records"][0]["dynamodb"]["NewImage"]
-    imie = image["PK"]["S"]
-    ses_response = ses_send(settings.email_title, imie)
+    name = image["PK"]["S"]
+    print(event)
+    logger.info(event)
+    ses_response = ses_send(settings.email_title, name)
 
     return {"statusCode": 200, "body": json.dumps(ses_response)}
