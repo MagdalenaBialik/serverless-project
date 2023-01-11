8d2e07eb0dc8716dbc5e115710322add3f3b13a2
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-01-11 19:42:15 +0700

    added statistic_class.py and class Statistic with methods for statistics

--- handlers/mail_statistics.py
+++ handlers/mail_statistics.py
@@ -12,12 +12,14 @@ def handler(event, context):
     db_table = boto3.resource(service_name="dynamodb", region_name="eu-west-1").Table(
         settings.dynamodb_table_name
     )
-    dao = DynamoDBDao(db_table, settings)
+    dao = DynamoDBDao(dynamodb_table=db_table, settings=settings)
 
     s3 = boto3.client(service_name="s3", region_name="eu-west-1")
     ses_client = boto3.client(service_name="ses", region_name="eu-west-1")
 
-    statistics_object = Statistic(db_table, s3, ses_client, settings)
+    statistics_object = Statistic(
+        dynamodb_table=db_table, s3_bucket=s3, ses_service=ses_client, settings=settings
+    )
 
     def statistics():
         pet_events = dao.get_all_pet_event(days=settings.days)
@@ -25,5 +27,6 @@ def handler(event, context):
         statistics_object.ses_send(settings.email_title, message_to_send)
         return message_to_send
 
-    statistics()
+        statistics()
+
     return {"statusCode": 200, "body": json.dumps("Hello from lambda")}
