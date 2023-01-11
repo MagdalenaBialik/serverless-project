e989a56a2c8e846958d9fcafc142b6123006f4ff
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-01-11 21:52:41 +0700

    updated

--- app/statistic_class.py
+++ app/statistic_class.py
@@ -1,6 +1,8 @@
 import operator
 from typing import List, Optional
 
+import boto3
+
 from app.base import SharedSettings
 from app.dynamodb_dao import DynamoDBDao
 from app.models import PetStatistics
@@ -15,17 +17,20 @@ class StatisticsSettings(SharedSettings):
 class Statistic:
     def __init__(
         self,
-        dynamodb_table,
         s3_client,
         ses_service,
         settings: StatisticsSettings,
-        dynamodb_dao: DynamoDBDao,
     ):
-        self.dynamodb_table = dynamodb_table
         self.s3_client = s3_client
         self.ses_service = ses_service
         self.settings = settings
-        self.dynamodb_dao = dynamodb_dao
+
+        self.dynamodb_table = boto3.resource(
+            service_name="dynamodb", region_name="eu-west-1"
+        ).Table(self.settings.dynamodb_table_name)
+        self.dynamodb_dao = DynamoDBDao(
+            dynamodb_table=self.dynamodb_table, settings=settings
+        )
 
     def get_presigned_url(self, pet_statistics: List[PetStatistics]):
         max_pet_statistics = max(pet_statistics, key=operator.attrgetter("count"))
--- handlers/mail_statistics.py
+++ handlers/mail_statistics.py
@@ -2,25 +2,17 @@ import json
 
 import boto3
 
-from app.dynamodb_dao import DynamoDBDao
 from app.statistic_class import Statistic, StatisticsSettings
 
 settings = StatisticsSettings()
 
-db_table = boto3.resource(service_name="dynamodb", region_name="eu-west-1").Table(
-    settings.dynamodb_table_name
-)
-
-dao = DynamoDBDao(dynamodb_table=db_table, settings=settings)
 s3 = boto3.client(service_name="s3", region_name="eu-west-1")
 ses_client = boto3.client(service_name="ses", region_name="eu-west-1")
 
 statistics_object = Statistic(
-    dynamodb_table=db_table,
     s3_client=s3,
     ses_service=ses_client,
     settings=settings,
-    dynamodb_dao=dao,
 )
 
 
