24b90606056ff6b404f200f068392f5a1e609a9e
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-01-12 20:06:08 +0700

    changed name from test_utils.py for test_stream.py and added new file conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import os
 
 import boto3
 import pytest
-from moto import mock_ses
 
 from app.stream_class import Stream
 
@@ -48,16 +47,3 @@ def test_event():
             }
         ]
     }
-
-
-def test_get_pet_name_from_stream_event(stream, test_event):
-    response = stream.get_pet_name_from_stream_event(test_event)
-    assert response == "Milusia"
-
-
-@mock_ses
-def test_send_mail_from_stream(ses_client, stream, test_event):
-    ses_client.verify_email_identity(EmailAddress="magdalena.bialik@gmail.com")
-
-    response = stream.send_mail_from_stream(title="Title", event=test_event)
-    assert response == "Milusia"
