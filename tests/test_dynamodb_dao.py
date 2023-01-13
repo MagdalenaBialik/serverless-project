407af7498cc5806654a4a0a53892e40f83876135
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-01-13 17:23:13 +0700

    added test dynamodb_dao

--- tests/test_dynamodb_dao.py
+++ tests/test_dynamodb_dao.py
@@ -11,8 +11,4 @@ def test_add_pet(dynamodb_dao):
 def test_add_random_pet(dynamodb_table, dynamodb_dao, shared_settings):
     dynamodb_dao.add_random_pet()
     response = dynamodb_dao.get_all_pet_event(None)
-    value = 0
-    for object in response:
-        if object.count == 1:
-            value = 1
-    assert value == 1
+    assert sum([object.count for object in response]) == 1
