787ab3c1a4c36da9fa24cb441aa7a4128bec449c
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-01-17 17:17:54 +0700

    updated

--- app/statistic_class.py
+++ app/statistic_class.py
@@ -27,7 +27,6 @@ class Statistic:
         max_pet_statistics = max(pet_statistics, key=operator.attrgetter("count"))
         s3_bucket_dao = S3BucketDAO(s3_client=self.s3_client, settings=self.settings)
         object_key = s3_bucket_dao.choose_rand_object_from_s3_bucket(max_pet_statistics)
-        # object_key = f"{max_pet_statistics.pet_name}.jpg"
 
         url = self.s3_client.generate_presigned_url(
             "get_object",
