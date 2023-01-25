44ad28779187301f47b75a0de4b3943d097abaff
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-01-25 15:37:53 +0700

    added model EventBriddgeEvent

--- app/models.py
+++ app/models.py
@@ -4,3 +4,13 @@ from pydantic import BaseModel
 class PetStatistics(BaseModel):
     pet_name: str
     count: int
+
+
+class EventBridgeEvent(BaseModel):
+    days: int
+    email_title: str
+
+
+# dict = {'days': '7', 'email_title': 'Pet of the days weekly statistics'}
+# object = EventBridgeEvent.parse_obj(dict)
+# print(object.days)
--- handlers/mail_statistics.py
+++ handlers/mail_statistics.py
@@ -1,12 +1,16 @@
 import json
 
 from app.base import SharedSettings
+from app.models import EventBridgeEvent
 from app.statistic_class import Statistic
 
 
 def handler(event, context):
     settings = SharedSettings()
     statistics_object = Statistic.create(settings)
-    statistics_object.send_statistics(days=event["days"], title=event["email_title"])
+    event_object = EventBridgeEvent.parse_obj(event)
+    statistics_object.send_statistics(
+        days=event_object.days, title=event_object.email_title
+    )
 
     return {"statusCode": 200, "body": json.dumps("Hello from lambda")}
