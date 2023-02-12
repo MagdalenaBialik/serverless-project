2837ae51cf5e812ddd32a6ec8c14f97f13f05bd9
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-02-12 11:52:25 +0100

    test validator in models.py

--- app/models.py
+++ app/models.py
@@ -11,11 +11,3 @@ class PetStatistics(BaseModel):
 class EventBridgeEvent(BaseModel):
     days: Optional[int]
     email_title: str
-    #
-    # @validator("days", pre=True)
-    # def allow_none(cls, v):
-    #     # TODO: check if this is necessary
-    #     if v is None:
-    #         return None
-    #     else:
-    #         return v
